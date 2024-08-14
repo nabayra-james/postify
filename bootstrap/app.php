fff6155b02e1e5a026abaeaafccde1318662ded1
Nabayra, James V. <146575214+nabayra-james@users.noreply.github.com>
2024-08-14 14:26:23 +0800

    modify app back-end

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\PreventBackHistory;
+use App\Http\Middleware\UserAuthenticate;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,8 +13,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(PreventBackHistory::class);
+        $middleware->alias([
+            'auth' => UserAuthenticate::class,
+            'prevent-back-history' => PreventBackHistory::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+    })
+    ->create();
